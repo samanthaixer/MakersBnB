87822c620e3bcbb1521abb29cf69233836e1dca0
Lauren <laurenqurashi@me.com>
2019-04-09 12:24:47 +0100

    Adding the database into our code and tests. Includes truncating test database.

--- spec/rspec/unit/nest_spec.rb
+++ spec/rspec/unit/nest_spec.rb
@@ -2,22 +2,25 @@
 require 'nest.rb'
 
 describe Nest do
-  let(:subject) { Nest.new(name: "Mongoose Manor", description: "The loveliest comfiest manor of all", nightly_rate: "200.00") }
+
+  before(:each) do
+    @result = DatabaseConnection.query("insert into nests (name, description, nightlyrate) values ('Mongoose Manor', 'The loveliest, comfiest manor of all', 200.00) returning id;")
+    @result2 = DatabaseConnection.query("insert into nests (name, description, nightlyrate) values ('Foliage Flat', 'A beautiful flat full of green', 130.00) returning id;")
+  end
 
   it 'returns the name of the nest' do
-    expect(subject.details).to include(description: "The loveliest comfiest manor of all")
+    expect(Nest.details(@result[0]['id'])).to have_attributes(description: "The loveliest, comfiest manor of all")
   end
 
   it 'returns Mongoose Manor for the name key' do
-    expect(subject.details).to include(:name => "Mongoose Manor")
+    expect(Nest.details(@result[0]['id'])).to have_attributes(:name => "Mongoose Manor")
   end
 
   it 'returns the correct details of a different nest' do
-    nest1 = Nest.new(name: "foliage flat", description: "A beautiful flat full of green", nightly_rate: "130.00")
-    expect(nest1.details).to include(:name => "foliage flat")
+    expect(Nest.details(@result2[0]['id'])).to have_attributes(:name => "Foliage Flat")
   end
 
   it 'returns a nightly rate' do
-    expect(subject.details).to include(nightly_rate: "200.00")
+    expect(Nest.details(@result[0]['id'])).to have_attributes(nightly_rate: "200.00")
   end
 end
