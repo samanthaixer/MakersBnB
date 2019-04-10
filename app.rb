118cb4479822b9d54f65015e56da55345b63f582
Lauren <laurenqurashi@me.com>
2019-04-10 17:10:56 +0100

    Messing around with jquery get request

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ require './spec/rspec/database_connection_setup.rb'
 class RentABurrow < Sinatra::Base
 
   get '/rentaburrow' do
-    puts Nest.details_json(1);
+    Nest.details_json(1);
   end
 
-  # get '/' do
-  #   send_file File.join(settings.public_folder, 'index.html')
-  # end
+  get '/' do
+    send_file File.join(settings.public_folder, 'index.html')
+  end
 
 end
--- public/src/scripts/rentaburrow.js
+++ public/src/scripts/rentaburrow.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 //            $(document.createElement('li')).text(item)
 //        );
 //    });
-   
+
 
 
 
@@ -37,3 +37,35 @@ $(document).ready(function() {
 //         $('#heading').val("Click!");
 //     }
 // });
+
+$(document).ready(function() {
+    // $('#testButton').click(function() {
+    //     console.log("Click!")
+    //     updateTest();
+    //   });
+    //
+    // function updateTest() {
+    //     $('#heading').val("Click!");
+    // }
+    $('#testButton').click(function() {
+      $.get('https://async-workshops-api.herokuapp.com/people', function(response) {
+        // (response => response.json)
+        // .then(response)
+
+        console.log("Is this here?")
+        practice = response
+        // obj = JSON.parse(practice);
+
+        console.log(practice[3]);
+        // console.log(nests.description);
+        // console.log(nests.nightlyrate);
+
+        //   var nests = JSON.parse(this.responseText);
+        // document.getElementById("?").innerHTML = nests.name;
+      })
+    });
+});
+
+
+// current code works when using the herokuapp site, the code under the get request
+// is recoomended by ellie.
