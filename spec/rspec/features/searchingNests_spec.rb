93c1b2934add2b0caa36bde34f619b996a04a87d
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-04-11 10:22:44 +0100

    Commented out feature tests

--- spec/rspec/features/searchingNests_spec.rb
+++ spec/rspec/features/searchingNests_spec.rb
@@ -1,7 +1,7 @@
-xfeature 'Viewing nest information' do
-  scenario 'when button clicked' do
-    visit '/rentaburrow'
-    click_button 'Get Stuff'
-    expect(page).to have_content "Add stuff here"
-  end
-end
+# feature 'Viewing nest information' do
+#   scenario 'when button clicked' do
+#     visit '/rentaburrow'
+#     click_button 'Get Stuff'
+#     expect(page).to have_content "Add stuff here"
+#   end
+# end
