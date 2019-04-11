a3190fbc3b0cc53806e4da2f0196dd936fa574c2
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-04-11 10:07:20 +0100

    getJSON call to our lovely API

--- app.rb
+++ app.rb
@@ -6,7 +6,8 @@ require './spec/rspec/database_connection_setup.rb'
 class RentABurrow < Sinatra::Base
 
   get '/rentaburrow' do
-    Nest.details_json(1);
+    return '[{"id":0,"name":"Bart"},{"id":1,"name":"Lauren"},{"id":2,"name":"Henry"},{"id":3,"name":"Sam"}]'
+    # puts Nest.details_json(1);
   end
 
   get '/' do
---
+++ spec/rspec/features/searchingNests_spec.rb
@@ -0,0 +1,7 @@
+xfeature 'Viewing nest information' do
+  scenario 'when button clicked' do
+    visit '/rentaburrow'
+    click_button 'Get Stuff'
+    expect(page).to have_content "Add stuff here"
+  end
+end
